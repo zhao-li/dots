3dfff1d69519b1715cb43a442958e982e81e14d5
Z Li <z.li@z.li>
2015-04-27 07:50:40 -1000

    order function to front

--- docker.sh
+++ docker.sh
@@ -1,16 +1,22 @@
 export DOCKER_HOST=tcp://localhost:4243
 
-DAY_OF_WEEK=$(date +"%a")
-if [ "$DAY_OF_WEEK" == "Mon" ]; then
-  echo "Remove extraneous Docker images and containers?"
-  select yn in "Yes" "No"; do
-      case $yn in
-          Yes ) 
-            wget -qO- https://raw.githubusercontent.com/blueyed/dotfiles/master/usr/bin/docker-cleanup | sh
-            break;;
-          No ) break;;
-      esac
-  done
-else
-  echo "Today isn't Monday so we are not cleaning up Docker"
+function clean_docker {
+  wget -qO- https://raw.githubusercontent.com/blueyed/dotfiles/master/usr/bin/docker-cleanup | sh
+}
+
+if [[ $- == *i* ]]; then # if interactive
+  DAY_OF_WEEK=$(date +"%a")
+  if [ "$DAY_OF_WEEK" == "Mon" ]; then
+    echo "Remove extraneous Docker images and containers?"
+    select yn in "Yes" "No"; do
+        case $yn in
+            Yes ) 
+              clean_docker
+              break;;
+            No ) break;;
+        esac
+    done
+  else
+    echo "Today isn't Monday so we are not cleaning up Docker"
+  fi
 fi
