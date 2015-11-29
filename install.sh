394a97529b9b61379f2aaa27be0a1b996f3ca36d
Zhao Li <zhao.li@startingspark.com>
2015-11-29 10:32:44 -1000

    programmatically setup symlinks

--- install.sh
+++ install.sh
@@ -1,7 +1,21 @@
-sudo apt-get update && apt-get install -y \
-  git \
-  gitk \
-  gitg \
-  tmux \
-  vim \
-  vim-gnome
+function sym_link() {
+  file=$1
+  path=$2
+  ln -sf $path/$file ~/$file
+}
+
+#sudo apt-get update && apt-get install -y \
+#  git \
+#  gitk \
+#  gitg \
+#  tmux \
+#  vim \
+#  vim-gnome
+
+path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+sym_link .bashrc       $path
+sym_link .bash_profile $path
+sym_link .tmux.conf    $path
+sym_link .vimrc        $path
+
