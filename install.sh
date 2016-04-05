68a99142620c03cec6c73acc10d63f10ad0c4469
Zhao Li <zhao.li@startingspark.com>
2016-04-04 15:56:54 -1000

    tweak script after getting ubuntu 14.04LTS up

--- install.sh
+++ install.sh
@@ -1,16 +1,20 @@
+#!/bin/bash
+
+# run script with sudo bash install.sh
+
 function sym_link() {
   file=$1
   path=$2
   ln -sf $path/$file ~/$file
 }
 
-#sudo apt-get update && apt-get install -y \
-#  git \
-#  gitk \
-#  gitg \
-#  tmux \
-#  vim \
-#  vim-gnome
+sudo apt-get update && apt-get install -y \
+  git \
+  gitk \
+  gitg \
+  tmux \
+  vim \
+  vim-gnome
 
 path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
