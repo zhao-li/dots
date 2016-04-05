e5799aa41db06f6e1b4190c74ab8def60702a1d2
Zhao Li <zhao.li@startingspark.com>
2016-04-04 16:04:35 -1000

    update to symlink based on operating system

--- install.sh
+++ install.sh
@@ -8,18 +8,26 @@ function sym_link() {
   ln -sf $path/$file ~/$file
 }
 
-sudo apt-get update && apt-get install -y \
-  git \
-  gitk \
-  gitg \
-  tmux \
-  vim \
-  vim-gnome
+#sudo apt-get update && apt-get install -y \
+#  git \
+#  gitk \
+#  gitg \
+#  tmux \
+#  vim \
+#  vim-gnome
 
 path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sym_link .bashrc       $path
-sym_link .bash_profile $path
-sym_link .tmux.conf    $path
-sym_link .vimrc        $path
+sym_link .tmux.conf $path
+sym_link .vimrc $path
+
+if [ "$(uname)" == "Darwin" ]; then
+  echo "doing mac stuff"
+  sym_link .bash_profile $path
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  echo "doing ubuntu stuff"
+  sym_link .bashrc $path
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+  echo "doing microsoft stuff"
+fi
 
