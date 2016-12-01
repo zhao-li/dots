16a63bb9e411a1f262ae4118eb0e9aa465c69d3b
Zhao Li <zhao.li@startingspark.com>
2016-11-30 20:42:43 -1000

    move notes into script since task seems to occur quite often

---
+++ fix_wifi.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "checking current installation:"
+dkms status
+
+echo "removing broken installation:"
+dkms remove -m rtl8812AU_8821AU_linux -v 1.0 --all
+
+echo "installing:"
+cp -R /tmp/rtl8812AU_8821AU_linux /usr/src/rtl8812AU_8821AU_linux-1.0 # probably still exists
+dkms add -m rtl8812AU_8821AU_linux -v 1.0
+dkms build -m rtl8812AU_8821AU_linux -v 1.0
+dkms install -m rtl8812AU_8821AU_linux -v 1.0
+
