e40679675890b3ba05e208e08d936b0746030a9d
Zhao Li <zhao.li@startingspark.com>
2019-01-23 16:56:58 -1000

    Images that were still useful were getting untagged and removed Getting rid of the —-all flag helped, but hopefully images older Than 6 months will still get removed.

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 function clean_docker {
   docker system prune --force
-  docker image prune --all --filter "until=4320h" --force # delete images older than 6 months ago
+  docker image prune --filter "until=4320h" --force # delete images older than 6 months ago
 }
