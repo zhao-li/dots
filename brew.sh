568d565e027c9a1e08be9e1fa888d86a1f83f56f
Zhao Li <zhao.li@startingspark.com>
2019-02-14 14:32:38 -1000

    fix indention

--- brew.sh
+++ brew.sh
@@ -4,7 +4,7 @@
 
 function setup_brew {
   if [ -f $(brew --prefix)/etc/bash_completion ]; then
-  . $(brew --prefix)/etc/bash_completion
+    . $(brew --prefix)/etc/bash_completion
   fi
   export PATH="/usr/local/bin:$PATH"
 }
