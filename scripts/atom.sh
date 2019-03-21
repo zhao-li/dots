3e693c9f8b297f4c5e2737f8478c44763128f88e
Zhao Li <zhao.li@startingspark.com>
2019-03-21 11:35:14 -1000

    leave a note to fix atom installation later

--- scripts/atom.sh
+++ scripts/atom.sh
@@ -3,5 +3,6 @@
 # This script and its functions supports atom.
 
 function install_atom_plugins() {
+  # sudo apm install --packages-file atomfile # switch to using sudo if new installation requires it
   apm install --packages-file atomfile
 }
