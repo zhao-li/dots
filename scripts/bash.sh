4e18dccbfb2e32fabd7e21134e77de709b73adeb
Zhao Li <zhao.li@startingspark.com>
2019-06-07 11:30:39 -1000

    group setting prompt with bash functions since the prompt will rely on a lot of bash dependencies to be fully functional

--- scripts/bash.sh
+++ scripts/bash.sh
@@ -13,7 +13,27 @@ function set_up_bash() {
 # This function loads base-it
 # example usage: load_bash_it
 function load_bash_it() {
-  if [ -f ~/dots/custom/bash-it.bash ]; then
+  echo "loading bash it"
+  if [ -d "~/.bash_it" ] && [ -f "~/dots/custom/bash-it.bash" ]; then
     source ~/dots/custom/bash-it.bash
   fi
+  set_prompt
+}
+
+# This function sets the prompt
+# example usage: set_prompt
+function set_prompt {
+  white="\[\e[00m\]"
+  green="\[\e[01;32m\]"
+  blue="\[\e[01;34m\]"
+  cyan="\[\e[1;36m\]"
+  user="\u"
+  working_directory="\w"
+
+  # could not find a way to fit on same line
+  PS1="💩$blue$user$green$working_directory$cyan"
+  if [ -d "~/.bash_it" ] && [ -f "~/dots/custom/bash-it.bash" ]; then
+    PS1+='$(__git_ps1 "(%s)")' 
+  fi
+  PS1+="$white$ "
 }
--- scripts/shell.sh
+++ scripts/shell.sh
@@ -5,26 +5,8 @@
 # This function customizes the shell
 # example usage: customize_shell
 function customize_shell {
-  set_prompt
   set_history_to_be_unique
 }
-
-# This function sets the prompt
-# example usage: set_prompt
-function set_prompt {
-  white="\[\e[00m\]"
-  green="\[\e[01;32m\]"
-  blue="\[\e[01;34m\]"
-  cyan="\[\e[1;36m\]"
-  user="\u"
-  working_directory="\w"
-
-  # could not find a way to fit on same line
-  PS1="💩$blue$user$green$working_directory$cyan"
-  PS1+='$(__git_ps1 "(%s)")' 
-  PS1+="$white$ "
-}
-
 
 # This function cleans up the shell history so it is not persisted
 # this way credentials typed into the shell history are not persisted
