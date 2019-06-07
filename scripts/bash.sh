19a6d06f7fcfcf16607808155941063cf0372130
Zhao Li <zhao.li@startingspark.com>
2019-06-07 13:30:04 -1000

    push setting prompt to separate file for better maintainability

--- scripts/bash.sh
+++ scripts/bash.sh
@@ -26,21 +26,3 @@ function load_bash_it() {
   fi
   set_prompt
 }
-
-# This function sets the prompt
-# example usage: set_prompt
-function set_prompt {
-  white="\[\e[00m\]"
-  green="\[\e[01;32m\]"
-  blue="\[\e[01;34m\]"
-  cyan="\[\e[1;36m\]"
-  user="\u"
-  working_directory="\w"
-
-  # could not find a way to fit on same line
-  PS1="💩$blue$user$green$working_directory$cyan"
-  if [ -d "$HOME/.bash_it" ] && [ -f "$HOME/dots/custom/bash-it.sh" ]; then
-    PS1+='$(__git_ps1 "(%s)")' 
-  fi
-  PS1+="$white$ "
-}
