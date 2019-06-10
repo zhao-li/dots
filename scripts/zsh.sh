562237b9dab3ff42cef9ec2784006166a89a90d0
Zhao Li <zhao.li@startingspark.com>
2019-06-10 11:30:01 -1000

    install oh-my-zsh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -8,3 +8,9 @@ set_up_zsh() {
   project_path=$1
   sym_link .zshrc "$project_path"
 }
+
+# This function installs oh-my-zsh
+# example usage: install_oh_my_zsh
+install_oh_my_zsh() {
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
+}
