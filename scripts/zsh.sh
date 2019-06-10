9ae4badb42d41c932681236725714803d27fb5a6
Zhao Li <zhao.li@startingspark.com>
2019-06-10 12:24:10 -1000

    load oh-my-zsh configurations if available

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -14,3 +14,12 @@ set_up_zsh() {
 install_oh_my_zsh() {
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
 }
+
+# This function loads oh-my-zsh
+# example usage: load_oh_my_zsh
+load_oh_my_zsh() {
+  if [ -d "$HOME/.oh-my-zsh" ] && [ -f "$HOME/dots/custom/oh-my-zsh.sh" ]; then
+    export ZSH_CUSTOM="$HOME/dots/custom/"
+    . ~/dots/custom/oh-my-zsh.sh
+  fi
+}
