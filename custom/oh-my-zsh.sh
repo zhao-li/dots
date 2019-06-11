db237de35e7c0710d3498a55dc6e387083575dea
Zhao Li <zhao.li@startingspark.com>
2019-06-10 15:04:10 -1000

    finalize oh-my-zsh prompt - push all code into a single file - use git-prompt plugin for convenience methods and variables

--- custom/oh-my-zsh.sh
+++ custom/oh-my-zsh.sh
@@ -71,6 +71,7 @@ ZSH_CUSTOM=$HOME/dots/custom/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 #plugins=(git)
+plugins=(git-prompt)
 
 source $ZSH/oh-my-zsh.sh
 
