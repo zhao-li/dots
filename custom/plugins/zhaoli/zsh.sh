d4744e6ed0d56282c3d83677156881afd8c822b5
Zhao Li <zhao.li@startingspark.com>
2019-06-11 11:42:25 -1000

    move loading oh-my-zsh to .zshrc to follow same pattern as loading bash-it

--- custom/plugins/zhaoli/zsh.sh
+++ custom/plugins/zhaoli/zsh.sh
@@ -20,12 +20,3 @@ install_oh_my_zsh() {
 
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh) --unattended"
 }
-
-# This function loads oh-my-zsh
-# example usage: load_oh_my_zsh
-load_oh_my_zsh() {
-  if [ -d "$HOME/.oh-my-zsh" ] && [ -f "$HOME/dots/custom/oh-my-zsh.sh" ]; then
-    export ZSH_CUSTOM="$HOME/dots/custom/"
-    . ~/dots/custom/oh-my-zsh.sh
-  fi
-}
