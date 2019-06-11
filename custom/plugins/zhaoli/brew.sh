652948abb4d3d0ba6306459a9b7509e83bf93f4a
Zhao Li <zhao.li@startingspark.com>
2019-06-11 10:58:21 -1000

    add comment and headers to brew file

--- custom/plugins/zhaoli/brew.sh
+++ custom/plugins/zhaoli/brew.sh
@@ -2,7 +2,12 @@
 
 # This script and its functions supports brew.
 
+# Load brew
+# Example usage: load_brew
 load_brew() {
+  about 'load brew'
+  group 'zhaoli'
+
   if [ -f "$(brew --prefix)"/etc/bash_completion ]; then
     # shellcheck disable=SC1090
     . "$(brew --prefix)"/etc/bash_completion
@@ -10,13 +15,23 @@ load_brew() {
   export PATH="/usr/local/bin:$PATH"
 }
 
+# Install brew packages
+# Example usage: install_brew_packages
 install_brew_packages() {
+  about 'install brew packages'
+  group 'zhaoli'
+
   xcode-select --install
   update_brew
   brew bundle
 }
 
+# Update brew and its packages
+# Example usage: update_brew
 update_brew() {
+  about 'update brew and its packages'
+  group 'zhaoli'
+
   brew update
   brew upgrade
   brew cask upgrade
