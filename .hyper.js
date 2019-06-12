b27434318c8240eceffd6225abf14c99c184e9a4
Zhao Li <zhao.li@startingspark.com>
2019-06-11 17:03:58 -1000

    configure hyper to have better pane navigation and solarized theme

--- .hyper.js
+++ .hyper.js
@@ -146,7 +146,10 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    "hyper-pane",
+    'hyper-solarized-dark',
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
