8c3d5ab76af05fca4ed392cca6f76edb4fcadfa5
Zhao Li <zhao.li@startingspark.com>
2019-06-18 16:37:35 -1000

    handle items that does not contain any uris key/value

--- custom/plugins/zhaoli/bitwarden.sh
+++ custom/plugins/zhaoli/bitwarden.sh
@@ -14,5 +14,5 @@ get_password() {
   username=$1
   site=$2
   session_hash=$3
-  bw list items --session "$session_hash" --search "$username" | jq ".[].login | select(.uris[].uri | contains(\"$site\")) | .password"
+  bw list items --session "$session_hash" --search "$username" | jq ".[].login | select(.uris[]?.uri | contains(\"$site\")) | .password"
 }
