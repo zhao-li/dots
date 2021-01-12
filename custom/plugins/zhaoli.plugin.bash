051996cfe50d81fc2ab941c6be2607a22a935df0
Zhao Li <zhao.li@startinsgpark.com>
2021-01-12 03:04:48 -1000

    revert back to the bash syntax; "find" within the shellcheck container behaves differently than "find" on the Mac host; the container "find" returns an iterable list of filenames whereas the host "find" returns a long string that is not iterated on

--- custom/plugins/zhaoli.plugin.bash
+++ custom/plugins/zhaoli.plugin.bash
@@ -1,13 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script loads the custom bash-it plugin
 
 cite about-plugin
 about-plugin "Helper functions for Zhao Li's typical workflow"
 
-files=$(find "$BASH_IT_CUSTOM"/plugins/zhaoli/ -type f )
-
-for file in $files; do
+for file in "$BASH_IT_CUSTOM"/plugins/zhaoli/*.{sh,bash}; do
   # shellcheck disable=SC1090 # these files will get checked by other means
   . "$file"
 done
--- custom/plugins/zhaoli/zhaoli.plugin.zsh
+++ custom/plugins/zhaoli/zhaoli.plugin.zsh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script loads the custom oh-my-zsh plugin
 
-for file in "$ZSH_CUSTOM"/plugins/zhaoli/*.sh; do
+for file in "$ZSH_CUSTOM"/plugins/zhaoli/*.{sh,bash}; do
   # shellcheck disable=SC1090 # these files will get checked by other means
   . "$file"
 done
+
