7ecc4c40b716cb2a7c09385a1eff42cae3ed5239
Zhao Li <zhao.li@startinsgpark.com>
2021-01-12 02:07:11 -1000

    refactor to be more robust

--- custom/plugins/zhaoli.plugin.bash
+++ custom/plugins/zhaoli.plugin.bash
@@ -5,7 +5,10 @@
 cite about-plugin
 about-plugin "Helper functions for Zhao Li's typical workflow"
 
-for file in "$BASH_IT_CUSTOM"/plugins/zhaoli/*.sh; do
+files=$(find "$BASH_IT_CUSTOM"/plugins/zhaoli/ -type f )
+
+for file in $files; do
   # shellcheck disable=SC1090 # these files will get checked by other means
   . "$file"
 done
+
